4d9d5544f25ea7f7a9b25ced1bf41c357663862f
Brian Wood <briwood@jackhenry.com>
2023-07-24 14:21:05 -0400

    Added test and volume credits function

--- src/statement.js
+++ src/statement.js
@@ -1,62 +1,67 @@
 function statement(invoice, plays) {
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = formatAmount();
-  for (const perf of invoice.performances) {
-    const play = plays[perf.playID];
-	volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    let thisAmount = calcAmount(perf, play);
+	let totalAmount = 0;
+	let volumeCredits = 0;
+	const format = formatAmount();
 
-    result += ` ${play.name}: ${format(thisAmount)} (${
-      perf.audience
-    } seats)\n`;
-    totalAmount += thisAmount;
-  }
-  result += `Amount owed is ${format(totalAmount)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
+	for (const perf of invoice.performances) {
+		const play = plays[perf.playID];
+		let thisAmount = calcAmount(perf, play);
+
+		totalAmount += thisAmount;
+		volumeCredits += calcVolumeCredits(perf, play);
+	}
+	for (const perf of invoice.performances) {
+		const play = plays[perf.playID];
+		let thisAmount = calcAmount(perf, play);
+
+		result += ` ${play.name}: ${format(thisAmount)} (${perf.audience} seats)\n`;
+	}
+
+  let result = `Statement for ${invoice.customer}\n`;
+	result += `Amount owed is ${format(totalAmount)}\n`;
+	result += `You earned ${volumeCredits} credits\n`;
+	return result;
 }
 
-function calcVolumeCredits(play, perf) {
+function calcVolumeCredits(perf, play) {
+	let volumeCredits = 0;
 	if ("comedy" === play.type) {
-		return Math.floor(perf.audience / 5);
+		volumeCredits += Math.floor(perf.audience / 5);
 	}
-	return Math.max(perf.audience - 30, 0);
+	return (volumeCredits += Math.max(perf.audience - 30, 0));
 }
 
 function calcAmount(perf, play) {
-  let thisAmount = 0;
-  switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-      throw new Error("unknown type: ${play.type}");
-  }
-  return calcDollarAmount(thisAmount);
+	let thisAmount = 0;
+	switch (play.type) {
+		case "tragedy":
+			thisAmount = 40000;
+			if (perf.audience > 30) {
+				thisAmount += 1000 * (perf.audience - 30);
+			}
+			break;
+		case "comedy":
+			thisAmount = 30000;
+			if (perf.audience > 20) {
+				thisAmount += 10000 + 500 * (perf.audience - 20);
+			}
+			thisAmount += 300 * perf.audience;
+			break;
+		default:
+			throw new Error("unknown type: ${play.type}");
+	}
+	return calcDollarAmount(thisAmount);
 }
 
 function calcDollarAmount(amount) {
-  return amount / 100;
+	return amount / 100;
 }
 
 function formatAmount() {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
+	return new Intl.NumberFormat("en-US", {
+		style: "currency",
+		currency: "USD",
+		minimumFractionDigits: 2,
+	}).format;
 }
-module.exports = { statement, calcAmount };
+module.exports = { statement, calcAmount, calcVolumeCredits };
--- src/statement.test.js
+++ src/statement.test.js
@@ -1,51 +1,63 @@
-const { statement, calcAmount } = require("./statement");
+const { statement, calcAmount, calcVolumeCredits } = require("./statement");
 
 describe("Statement Test", () => {
-  it("should test statement returns values", () => {
-    const plays = {
-      hamlet: { name: "Hamlet", type: "tragedy" },
-      "as-like": { name: "As You Like It", type: "comedy" },
-      othello: { name: "Othello", type: "tragedy" },
-    };
+	it("should test statement returns values", () => {
+		const plays = {
+			hamlet: { name: "Hamlet", type: "tragedy" },
+			"as-like": { name: "As You Like It", type: "comedy" },
+			othello: { name: "Othello", type: "tragedy" },
+		};
 
-    const invoices = [
-      {
-        customer: "BigCo",
-        performances: [
-          {
-            playID: "hamlet",
-            audience: 55,
-          },
-          {
-            playID: "as-like",
-            audience: 35,
-          },
-          {
-            playID: "othello",
-            audience: 40,
-          },
-        ],
-      },
-    ];
-    const expectedOutput =
-      "Statement for BigCo\n" +
-      " Hamlet: $650.00 (55 seats)\n" +
-      " As You Like It: $580.00 (35 seats)\n" +
-      " Othello: $500.00 (40 seats)\n" +
-      "Amount owed is $1,730.00\n" +
-      "You earned 47 credits\n";
-    expect(statement(invoices[0], plays)).toEqual(expectedOutput);
-  });
+		const invoices = [
+			{
+				customer: "BigCo",
+				performances: [
+					{
+						playID: "hamlet",
+						audience: 55,
+					},
+					{
+						playID: "as-like",
+						audience: 35,
+					},
+					{
+						playID: "othello",
+						audience: 40,
+					},
+				],
+			},
+		];
+		const expectedOutput =
+			"Statement for BigCo\n" +
+			" Hamlet: $650.00 (55 seats)\n" +
+			" As You Like It: $580.00 (35 seats)\n" +
+			" Othello: $500.00 (40 seats)\n" +
+			"Amount owed is $1,730.00\n" +
+			"You earned 47 credits\n";
+		expect(statement(invoices[0], plays)).toEqual(expectedOutput);
+	});
 
-  it("should calculate correct amount for tragedy plays", () => {
-    expect(
-      calcAmount(
-        {
-          playID: "hamlet",
-          audience: 55,
-        },
-        { name: "Hamlet", type: "tragedy" }
-      )
-    ).toEqual(650.00);
-  });
+	it("should calculate correct amount for tragedy plays", () => {
+		expect(
+			calcAmount(
+				{
+					playID: "hamlet",
+					audience: 55,
+				},
+				{ name: "Hamlet", type: "tragedy" }
+			)
+		).toEqual(650.0);
+	});
+
+	it("should calculate correct volume credits for comedy play", () => {
+		expect(
+			calcVolumeCredits(
+				{
+					playID: "hamlet",
+					audience: 55,
+				},
+				{ name: "Hamlet", type: "comedy" }
+			)
+		).toEqual(36);
+	});
 });
