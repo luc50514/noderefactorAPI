71e3fd933fa6d80b532b252350df02520b55f1c7
dbraaksma <dbraaksma@jackhenry.com>
2023-08-01 14:29:00 -0400

    successfully adding new play

--- src/statement.test.js
+++ src/statement.test.js
@@ -1,90 +1,114 @@
-const { statement, calcAmount, calcVolumeCredits, calculatePlayObjects } = require("./statement");
+const {
+  statement,
+  calcAmount,
+  calcVolumeCredits,
+  calculatePlayObjects,
+} = require("./statement");
 const plays = {
-	hamlet: { name: "Hamlet", type: "tragedy" },
-	"as-like": { name: "As You Like It", type: "comedy" },
-	othello: { name: "Othello", type: "tragedy" },
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
+  RomeoAndJuliet: { name: "RomeoAndJuliet", type: "tragedy" },
 };
 
 describe("Statement Test", () => {
-	it("should test statement returns values", () => {
-		const invoices = [
-			{
-				customer: "BigCo",
-				performances: [
-					{
-						playID: "hamlet",
-						audience: 55,
-					},
-					{
-						playID: "as-like",
-						audience: 35,
-					},
-					{
-						playID: "othello",
-						audience: 40,
-					},
-				],
-			},
-		];
-		const expectedOutput =
-			"Statement for BigCo\n" +
-			" Hamlet: $650.00 (55 seats)\n" +
-			" As You Like It: $580.00 (35 seats)\n" +
-			" Othello: $500.00 (40 seats)\n" +
-			"Amount owed is $1,730.00\n" +
-			"You earned 47 credits\n";
-		expect(statement(invoices[0], plays)).toEqual(expectedOutput);
-	});
+  it("should test statement returns values", () => {
+    const invoices = [
+      {
+        customer: "BigCo",
+        performances: [
+          {
+            playID: "hamlet",
+            audience: 55,
+          },
+          {
+            playID: "as-like",
+            audience: 35,
+          },
+          {
+            playID: "othello",
+            audience: 40,
+          },
+        ],
+      },
+    ];
+    const expectedOutput =
+      "Statement for BigCo\n" +
+      " Hamlet: $650.00 (55 seats)\n" +
+      " As You Like It: $580.00 (35 seats)\n" +
+      " Othello: $500.00 (40 seats)\n" +
+      "Amount owed is $1,730.00\n" +
+      "You earned 47 credits\n";
+    expect(statement(invoices[0], plays)).toEqual(expectedOutput);
+  });
 
-	it("should calculate correct amount for tragedy plays", () => {
-		expect(
-			calcAmount(
-				{
-					playID: "hamlet",
-					audience: 55,
-				},
-				{ name: "Hamlet", type: "tragedy" }
-			)
-		).toEqual(650.0);
-	});
-
-	it("should calculate correct volume credits for comedy play", () => {
-		expect(
-			calcVolumeCredits(
-				{
-					playID: "hamlet",
-					audience: 55,
-				},
-				{ name: "Hamlet", type: "comedy" }
-			)
-		).toEqual(36);
-	});
-
-  it("should return an array of play calculation objects", ()=>{
+  it("should calculate correct amount for tragedy plays", () => {
     expect(
-		calculatePlayObjects(
-			[
-				{
-					playID: "hamlet",
-					audience: 55,
-				},
-			],
-			plays
-		)
-	).toEqual([{ id: "hamlet", totalAmount: 650, volumeCredits: 25, thisAmount: 650, playName: "Hamlet", audience: 55}]);
-  })
+      calcAmount(
+        {
+          playID: "hamlet",
+          audience: 55,
+        },
+        { name: "Hamlet", type: "tragedy" }
+      )
+    ).toEqual(650.0);
+  });
 
-  it("Add a play", ()=>{
+  it("should calculate correct volume credits for comedy play", () => {
     expect(
-		calculatePlayObjects(
-			[
-				{
-					playID: "RomeoAndJuliet",
-					audience: 55,
-				},
-			],
-			plays
-		)
-	).toEqual([{ id: "hamlet", totalAmount: 650, volumeCredits: 25, thisAmount: 650, playName: "Hamlet", audience: 55}]);
-  })
+      calcVolumeCredits(
+        {
+          playID: "hamlet",
+          audience: 55,
+        },
+        { name: "Hamlet", type: "comedy" }
+      )
+    ).toEqual(36);
+  });
+
+  it("should return an array of play calculation objects", () => {
+    expect(
+      calculatePlayObjects(
+        [
+          {
+            playID: "hamlet",
+            audience: 55,
+          },
+        ],
+        plays
+      )
+    ).toEqual([
+      {
+        id: "hamlet",
+        totalAmount: 650,
+        volumeCredits: 25,
+        thisAmount: 650,
+        playName: "Hamlet",
+        audience: 55,
+      },
+    ]);
+  });
+
+  it("Add a play", () => {
+    expect(
+      calculatePlayObjects(
+        [
+          {
+            playID: "RomeoAndJuliet",
+            audience: 65,
+          },
+        ],
+        plays
+      )
+    ).toEqual([
+      {
+        id: "RomeoAndJuliet",
+        totalAmount: 750,
+        volumeCredits: 35,
+        thisAmount: 750,
+        playName: "RomeoAndJuliet",
+        audience: 65,
+      },
+    ]);
+  });
 });
