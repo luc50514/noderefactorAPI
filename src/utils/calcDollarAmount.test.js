173b19d1a4e887c56930a912d21482e92008e304
Juan Miranda <jmiranda@jackhenry.com>
2023-08-11 12:02:35 -0600

    Fix test for calcAmount

--- src/utils/calcDollarAmount.test.js
+++ src/utils/calcDollarAmount.test.js
@@ -1,11 +1,46 @@
 const { calcDollarAmount, calcAmount } = require("./calcDollarAmount.js");
 
 describe("Calc dollar amount", () => {
+	const invoices = [
+		{
+			customer: "BigCo",
+			performances: [
+				{
+					playID: "hamlet",
+					audience: 55,
+				},
+				{
+					playID: "as-like",
+					audience: 35,
+				},
+				{
+					playID: "othello",
+					audience: 40,
+				},
+			],
+		},
+	];
+
+	const plays = {
+		hamlet: { name: "Hamlet", type: "tragedy" },
+		"as-like": { name: "As You Like It", type: "comedy" },
+		othello: { name: "Othello", type: "tragedy" },
+		RomeoAndJuliet: { name: "RomeoAndJuliet", type: "tragedy" },
+	};
+
 	it("should divide amount by 100", () => {
 		expect(calcDollarAmount(100)).toEqual(1);
 	});
 
 	it("calculates play values based on play type", () => {
-		expect(calcAmount().toEqual(1));
+		
+
+		expect(calcAmount(invoices[0].performances[0],plays.hamlet)).toEqual(650.0);
+	});
+
+	it("calculates performance amount refactor", () => {
+		
+
+		expect(1).toEqual(650.0);
 	});
 });
